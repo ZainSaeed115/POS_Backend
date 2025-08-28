ff04ac1117c1bec42467c0281961b217fd9b4155
ZainSaeed115 <muhammadzainsaeed115@gmail.com>
2025-08-28 17:21:06 +0500

    add product statistic feature

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,33 +1,33 @@
 import express from "express";
 import { 
-    createProduct ,
+    createProduct,
     getProducts,
     getProductsById,
     updateProductById,
     deleteProductById,
     searchProduct,
     getProductByBarCode,
-    makeOffer
+    makeOffer,
+    getProductStatistic
 } from "../controllers/products.controller.js";
 import { upload } from "../middleware/multer.js";
-import {verifyJwt} from "../middleware/verifyToken.js"
+import { verifyJwt } from "../middleware/verifyToken.js"
 import { validateBody } from "../middleware/validateSchema.js";
-import { createProductSchema ,updateProductSchema} from "../utils/validations.js";
+import { createProductSchema, updateProductSchema } from "../utils/validations.js";
 
-const router=express.Router();
+const router = express.Router();
 
-router.post("/create",verifyJwt,upload.single("image")
-,createProduct);
-router.get("/get",verifyJwt,getProducts);
-router.get('/search',searchProduct)
-router.get("/:productId",verifyJwt,getProductsById);
-router.get('/barcode/:barcode', verifyJwt,getProductByBarCode);
-router.put("/update/:productId",verifyJwt,upload.fields([{
-    name:"image"
-}]),validateBody(updateProductSchema),updateProductById);
-router.delete("/delete/:productId",verifyJwt,deleteProductById);
+router.post("/create", verifyJwt, upload.single("image"), createProduct);
+router.get("/get", verifyJwt, getProducts);
+router.get("/statistics",verifyJwt,getProductStatistic)
+router.get('/search', searchProduct);
+router.get("/:productId", verifyJwt, getProductsById);
+router.get('/barcode/:barcode', verifyJwt, getProductByBarCode);
+router.put("/update/:productId", verifyJwt, upload.fields([{
+    name: "image"
+}]), validateBody(updateProductSchema), updateProductById);
+router.delete("/delete/:productId", verifyJwt, deleteProductById);
 router.post("/make-offer/:productId", verifyJwt, makeOffer);
 
 
 export default router;
-
